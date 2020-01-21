dcf1d471b577ae279083cc92ca03f738c31fdf29
Balu Abraham Kurian <kurianbalu@gmail.com>
2020-01-21 22:36:25 +0530

    Singleton

---
+++ src/main/java/hfdp/singleton/ChocolateBoiler.java
@@ -0,0 +1,46 @@
+package hfdp.singleton;
+
+public class ChocolateBoiler {
+    private boolean empty;
+    private boolean boiled;
+    private static ChocolateBoiler instance;
+    private ChocolateBoiler() {
+        empty = true;
+        boiled = false;
+    }
+
+    public static synchronized ChocolateBoiler getInstance() {
+        if(instance == null)
+            instance = new ChocolateBoiler();
+
+        return instance;
+    }
+
+    public void fill() {
+        if (isEmpty()) {
+            empty = false;
+            boiled = false;
+        }
+    }
+
+    public void drain() {
+        if (!isEmpty() && isBoiled()) {
+// drain the boiled milk and chocolate
+            empty = true;
+        }
+    }
+
+    public void boil() {
+        if (!isEmpty() && !isBoiled()) {
+            boiled = true;
+        }
+    }
+
+    public boolean isEmpty() {
+        return empty;
+    }
+
+    public boolean isBoiled() {
+        return boiled;
+    }
+}
